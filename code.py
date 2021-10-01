d61f0e49b44c523903e65e30711abeb3d56e0d38
Ritik <52455027+ritikgoyal189@users.noreply.github.com>
2021-10-01 22:19:07 +0530

    Update code.py

--- code.py
+++ code.py
@@ -1,1 +1,9 @@
 print("Your code goes here...")
+
+
+num = int(input("Enter a number: "))
+if (num % 2) == 0:
+   print("{0} is Even".format(num))
+else:
+   print("{0} is Odd".format(num))
+# thats it !!
